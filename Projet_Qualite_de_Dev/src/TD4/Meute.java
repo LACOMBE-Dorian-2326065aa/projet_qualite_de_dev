3293c3c3a687c1aa2bc621cbf65ba9f4a05c312e
CURT-Elien-2326035a <zeustheplayer12@hotmail.com>
2024-11-08 11:44:25 +0100

    algo de tri

--- Projet_Qualite_de_Dev/src/TD4/Meute.java
+++ Projet_Qualite_de_Dev/src/TD4/Meute.java
@@ -1,8 +1,6 @@
 package TD4;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class Meute {
     private String nom;
@@ -10,9 +8,7 @@ public class Meute {
     private ArrayList<Lycanthrope> meute;
     private String rang = "αβγδεζηθικλμνξοπρστυϕχψω";
 
-    
-
-    public void  addLycanthrope(Lycanthrope lycanthrope, Lycanthrope[] meute) {
+    public void addLycanthrope(Lycanthrope lycanthrope, Lycanthrope[] meute) {
         for (int i = 0; i < meute.length; i++) {
             if (meute[i] == null) {
                 meute[i] = lycanthrope;
@@ -34,17 +30,15 @@ public class Meute {
         return this.meute;
     }
 
-    public void orderMeute() {
-        Arrays.sort(meute, new Comparator<Lycanthrope>() {
-            @Override
-            public int compare(Lycanthrope l1, Lycanthrope l2) {
-                if (l1 == null && l2 == null) return 0;
-                if (l1 == null) return 1;
-                if (l2 == null) return -1;
-                return rang.indexOf(l1.getRangDomination()) - rang.indexOf(l2.getRangDomination());
+    public void orderMeute(ArrayList<Lycanthrope> meute) {
+        for (int i = 0; i < meute.size(); i++) {
+            for (int j = 0; j < meute.size(); j++) {
+                if (rang.indexOf(meute.get(i).getRangDomination()) < rang.indexOf(meute.get(j).getRangDomination())) {
+                    Lycanthrope temp = meute.get(i);
+                    meute.set(i, meute.get(j));
+                    meute.set(j, temp);
+                }
             }
-        });
+        }
     }
-
-    
 }
