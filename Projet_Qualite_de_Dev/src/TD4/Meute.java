51c2d0e992723ba622f5983540f33d413daefa34
CURT-Elien-2326035a <zeustheplayer12@hotmail.com>
2024-11-08 11:51:10 +0100

    fix

--- Projet_Qualite_de_Dev/src/TD4/Meute.java
+++ Projet_Qualite_de_Dev/src/TD4/Meute.java
@@ -8,19 +8,14 @@ public class Meute {
     private ArrayList<Lycanthrope> meute;
     private String rang = "αβγδεζηθικλμνξοπρστυϕχψω";
 
-    public void addLycanthrope(Lycanthrope lycanthrope, Lycanthrope[] meute) {
-        for (int i = 0; i < meute.length; i++) {
-            if (meute[i] == null) {
-                meute[i] = lycanthrope;
-                break;
-            }
-        }
+    public void addLycanthrope(Lycanthrope lycanthrope, ArrayList<Lycanthrope> meute) {
+        meute.add(lycanthrope);
     }
 
-    public void removeLycanthrope(Lycanthrope lycanthrope, Lycanthrope[] meute) {
-        for (int i = 0; i < meute.length; i++) {
-            if (meute[i] == lycanthrope) {
-                meute[i] = null;
+    public void removeLycanthrope(Lycanthrope lycanthrope, ArrayList<Lycanthrope> meute) {
+        for (int i = 0; i < meute.size(); i++) {
+            if (meute.get(i) == lycanthrope) {
+                meute.remove(i);
                 break;
             }
         }
